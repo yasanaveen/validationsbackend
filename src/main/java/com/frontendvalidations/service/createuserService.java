a4091744e62b38b6a1d77042e11193ca4ceceb08
Naveen Yasa <yasanaveen535@gmail.com>
2025-04-15 12:38:51 +0530

    new one

--- src/main/java/com/frontendvalidations/service/createuserService.java
+++ src/main/java/com/frontendvalidations/service/createuserService.java
@@ -1,6 +1,5 @@
 package com.frontendvalidations.service;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.frontendvalidations.entity.Login;
